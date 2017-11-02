fca4e5773123e29ea8ff8cfe6c12d0dafdaf1d31
Stewart ilondanga <stilondanga@gmail.com>
2017-11-02 11:44:23 +0300

    add index.html

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".dropdown-menu li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
